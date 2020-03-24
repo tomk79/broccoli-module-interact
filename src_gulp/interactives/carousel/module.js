32d24e57a4a8f81df685366be0ce4d4e0e7597bf
Tomoya Koyanagi <tomk79@gmail.com>
2020-03-24 23:05:43 +0900

    1コンテンツ内に複数のカルーセルを配置した場合に、2つ目以降のカルーセルが動作しない不具合を修正。

--- src_gulp/interactives/carousel/module.js
+++ src_gulp/interactives/carousel/module.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', function(e){
-	var carouselModules = document.querySelectorAll('.px2-carousel');
+	var carouselModules = document.querySelectorAll('.px2-carousel .px2-carousel__contents');
 	for(var idx = 0; idx < carouselModules.length; idx ++){
 		// console.log(idx);
+		tns({
+			container: carouselModules[idx],
+			nav: true,
+			controlsText: ['＜', '＞']
+		});
 	}
-	tns({
-		container: '.px2-carousel .px2-carousel__contents',
-		nav: true,
-		controlsText: ['＜', '＞']
-	});
 });
